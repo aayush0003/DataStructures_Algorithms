7aa55637e3d2501f30e3f3b2da406fdb12879ab6
aayush0003 <46254746+aayush0003@users.noreply.github.com>
2021-06-04 02:26:49 +0530

    Update Branch_Sums.py

--- solutions/Branch_Sums.py
+++ solutions/Branch_Sums.py
@@ -1,6 +1,9 @@
 
 """
-2) Write a function that takes in a Binary Tree and returns a list of its branch sums (ordered from leftmost branch sum to rightmost branch sum). A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree branch is a path of nodes in a tree that starts at the root node and ends at any leaf node. Each Binary Tree node has a value stored in a property called "value" and two children nodes stored in properties called "left" and "right," respectively. Children nodes can either be Binary Tree nodes themselves or the None (null) value.
+2) Write a function that takes in a Binary Tree and returns a list of its branch sums (ordered from leftmost branch sum to rightmost branch sum).
+A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree branch is a path of nodes in a tree that starts at the root node and
+ends at any leaf node. Each Binary Tree node has a value stored in a property called "value" and two children nodes stored in properties called "left" and "right,"
+respectively. Children nodes can either be Binary Tree nodes themselves or the None (null) value.
 
 # Add, edit, or remove tests in this file.
 # Treat it as your playground!
