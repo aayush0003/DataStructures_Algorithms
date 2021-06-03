3c7ee99f512b0c318817b9a236617fadfe95879a
aayush0003 <46254746+aayush0003@users.noreply.github.com>
2021-06-04 02:29:28 +0530

    Day 1

--- solutions/nthfibonacci.py
+++ solutions/nthfibonacci.py
@@ -1,6 +1,7 @@
 """
 Question:
-The Fibonacci sequence is defined as follows: the first number of the sequence is 0, the second number is 1, and the nth number is the sum of the (n - 1)th and (n - 2)th numbers. Write a function that takes in an integer n and returns the nth Fibonacci number.
+3)The Fibonacci sequence is defined as follows: the first number of the sequence is 0, the second number is 1, 
+and the nth number is the sum of the (n - 1)th and (n - 2)th numbers. Write a function that takes in an integer n and returns the nth Fibonacci number.
 
 # Add, edit, or remove tests in this file.
 # Treat it as your playground!
