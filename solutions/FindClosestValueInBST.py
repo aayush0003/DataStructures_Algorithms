daaa17c46a5aea143c3634959d0b8cfe6508c695
aayush0003 <46254746+aayush0003@users.noreply.github.com>
2021-06-04 02:31:13 +0530

    Day 1

--- solutions/FindClosestValueInBST.py
+++ solutions/FindClosestValueInBST.py
@@ -1,5 +1,9 @@
-﻿"""
-You are given a BST data structure consisting of BST nodes. Each BST node has an integer value stored in a property called "value" and two children nodes stored in properties called "left" and "right," respectively. A node is said to be a BST node if and only if it satisfies the BST property: its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; and both of its children nodes are either BST nodes themselves or None (null) values. You are also given a target integer value; write a function that finds the closest value to that target value contained in the BST. Assume that there will only be one closest value.
+"""
+4)You are given a BST data structure consisting of BST nodes. Each BST node has an integer value stored in a property called "value"
+and two children nodes stored in properties called "left" and "right," respectively. A node is said to be a BST node if and only if it satisfies the BST property:
+its value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; and both of its
+children nodes are either BST nodes themselves or None (null) values. You are also given a target integer value; write a function that finds the closest value to that
+target value contained in the BST. Assume that there will only be one closest value.
 
 # Add, edit, or remove tests in this file.
 # Treat it as your playground!
@@ -98,7 +102,7 @@ if __name__ == "__main__":
 
 """
 #Solution
-​# Average: O(log(n)) time | O(1) space
+# Average: O(log(n)) time | O(1) space
 # Worst: O(n) time | O(1) space
 def findClosestValueInBst(tree, target):
     return findClosestValueInBstHelper(tree, target, float("inf"))
